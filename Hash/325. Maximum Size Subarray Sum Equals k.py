2e22ccdcc6c6caa2c432fc360950d55a2731ec67
Du Zhang <duz1@andrew.cmu.edu>
2017-09-29 03:01:22 -0400

    2017.09.28

---
+++ Hash/325. Maximum Size Subarray Sum Equals k.py
@@ -0,0 +1,41 @@
+# Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+
+# Note:
+# The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+
+# Example 1:
+# Given nums = [1, -1, 5, -2, 3], k = 3,
+# return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
+
+# Example 2:
+# Given nums = [-2, -1, 2, 1], k = 1,
+# return 2. (because the subarray [-1, 2] sums to 1 and is the longest)
+
+# Follow Up:
+# Can you do it in O(n) time?
+
+
+# clarify: the subarray must be continious
+# idea: use a hash table in which current sum is the key, current index is the value.
+# If current value minus a previous one in the hash table, then we get one possible answer.
+# Time:  O(n)
+# Space: O(n)
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sums = {}
+        cur_sum, max_len = 0, 0
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            if cur_sum not in sums:
+                sums[cur_sum] = i
+            if cur_sum == k:
+                max_len = i + 1
+            elif (cur_sum - k) in sums:
+                max_len = max(max_len, i - sums[cur_sum - k])
+            
+        return max_len
---
+++ Queue/621. Task Scheduler.py
@@ -0,0 +1,58 @@
+# Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks.Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+
+# However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
+
+# You need to return the least number of intervals the CPU will take to finish all the given tasks.
+
+# Example 1:
+# Input: tasks = ['A','A','A','B','B','B'], n = 2
+# Output: 8
+# Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+# Note:
+# The number of tasks is in the range [1, 10000].
+# The integer n is in the range [0, 100].
+
+import queue
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        # count the frequency of each task
+        count_freq = {}
+        for task in tasks:
+            if task not in count_freq:
+                count_freq[task] = 1
+            else:
+                count_freq[task] += 1
+        
+        # put the frequency of each task into a priority queue. largest frequency should be in the front of the queue.
+        # use '-' here because PriorityQueue in python can only sort ascendingly. We want it decendingly.
+        freq_queue = queue.PriorityQueue()
+        for task in count_freq.keys():
+            freq_queue.put(-count_freq[task])
+            
+        intervals = 0
+        cycle = n + 1
+        while not freq_queue.empty():
+            tmp_freq, time = [], 0
+            # the pop out cycle, we pop out the task with largest frequency.
+            # need to store it in a tmp list, because we may insert it back again.
+            for _ in range(cycle):
+                if not freq_queue.empty():
+                    tmp_freq.append(freq_queue.get())
+                    time += 1
+                else:
+                    break
+            # if the frequency - 1 != 0, we must insert it to the PriorityQueue again
+            for freq in tmp_freq:
+                freq += 1
+                if freq:
+                    freq_queue.put(freq)
+            
+            intervals += cycle if not freq_queue.empty() else time
+        
+        return intervals
+          
